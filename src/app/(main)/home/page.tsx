ed473531ef95cb90655ae4b94e0fbb6cb54862d9
Habibtusher <tusherhrt@gmail.com>
2023-10-31 22:58:32 +0600

    responsive img

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <div>
       <div
-        className="bg-cover bg-center h-96 md:h-[500px] lg:h-[600px] xl:h-screen 2xl:h-screen bg-opacity-50 text-white flex items-center justify-center"
+        className="bg-cover bg-center h-64 md:h-[500px] lg:h-[600px] xl:[700px] 2xl:h-screen bg-opacity-50 text-white flex items-center justify-center"
         style={{
           backgroundImage: "url('/bb.png')",
         }}
