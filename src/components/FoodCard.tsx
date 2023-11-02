fcf2087d5594f5b034288d81415ce53f9d828fa8
Habibtusher <tusherhrt@gmail.com>
2023-11-02 21:04:05 +0600

    Food card design

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -12,14 +12,13 @@ import React, { useState } from "react";
 import { MinusCircleOutlined, PlusCircleOutlined } from "@ant-design/icons";
 
 interface Props {
-  food: IFood; 
+  food: IFood;
 }
 const FoodCard: React.FC<Props> = ({ food }) => {
   const { cart, total } = useAppSelector((state) => state.service);
   const dispatch = useAppDispatch();
 
   const handleAddToCart = (product: any) => {
-   
     dispatch(addToCart(product));
   };
 
@@ -28,40 +27,44 @@ const FoodCard: React.FC<Props> = ({ food }) => {
   const quantityInCart = cartItem ? cartItem.quantity : 0;
   const des =
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, nemo! Vel blanditiis voluptates quam ipsam nam nemo dolorum voluptatem dignissimos ducimus nihil. Fuga exercitationem eveniet inventore deserunt pariatur tenetur necessitatibus cumque dolor asperiores odit magnam quis, id delectus quisquam ducimus, consectetur distinctio fugiat dignissimos? Itaque provident dolor labore non hic. ";
-  const description = des && des.length > 100 ? `${des.slice(0, 100)}...` : des;
+  const description = des && des.length > 80 ? `${des.slice(0, 80)}...` : des;
   return (
-    <div className="bg-white shadow-md rounded-lg p-2 pb-5 w-full">
-      <div className="text-center mb-4">
-        <img src={food.image} alt={food.name} className="h-32 mx-auto mb-2" />
-        <p className="font-semibold">{food.name}</p>
-        <p className="text-gray-500">{food?.category?.name}</p>
-        <p className="text-lg font-bold">$ {food.price}</p>
-        <p className="text-base "> {description} </p>
-      </div>
-      {cartItem ? (
-        <div className="text-center font-semibold">
-          <div className="flex items-center justify-around  text-white rounded-full px-4 py-2 w-full bg-[#44331e] ">
-            <MinusCircleOutlined
-              className="minus"
-              onClick={() => dispatch(decreaseQuantity(food?._id))}
-            />
-            {quantityInCart}
-            <PlusCircleOutlined
-              onClick={() => dispatch(incrementQuantity(food?._id))}
-              className="plus"
-            />
-          </div>
+    <div className="card glass shadow-xl">
+      <figure>
+        <img className="max-h-40" src={food.image} alt="car!" />
+      </figure>
+      <div className="card-body items-center text-center p-4">
+        <p className="card-title">{food.name}</p>
+        <p>$ {food.price}</p>
+        <p>{description}</p>
+        
+          {cartItem ? (
+            <div className="text-center font-semibold w-full">
+              <div className="flex items-center justify-around  text-white rounded-full px-4 py-2 w-full bg-[#44331e] ">
+                <MinusCircleOutlined
+                  className="minus"
+                  onClick={() => dispatch(decreaseQuantity(food?._id))}
+                />
+                {quantityInCart}
+                <PlusCircleOutlined
+                  onClick={() => dispatch(incrementQuantity(food?._id))}
+                  className="plus"
+                />
+              </div>
+            </div>
+          ) : (
+            <button
+              onClick={() => handleAddToCart(food)}
+              //       // disabled={addedToCart}
+              className="bg-[#EA972D] text-white rounded-full font-semibold px-4 py-2 w-full hover:bg-[#44331e]"
+            >
+              Add to Cart
+            </button>
+          )}
         </div>
-      ) : (
-        <button
-          onClick={() => handleAddToCart(food)}
-          // disabled={addedToCart}
-          className="bg-[#EA972D] text-white rounded-full font-semibold px-4 py-2 w-full hover:bg-[#44331e]"
-        >
-          Add to Cart
-        </button>
-      )}
-    </div>
+      </div>
+   
+    
   );
 };
 
